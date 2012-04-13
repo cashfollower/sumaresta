5cfb450d785fbf2144c2e44fb3fbe0d838fbd290
Álvaro Durán Tovar <hermesdt@gmail.com>
2012-04-13 17:14:47 +0200

    Added new empty rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sumaresta::Application.config.secret_token = '392713fd6da10936755c178ee351c12161845338e4f0a5e58834c0f0341eecebe3e593994f53f1598caf32a46500e37ca6260b555d58a272ec258c15eddc3b99'
